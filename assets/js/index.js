24fe70571292c6dfd3aff25676d9d8db4a6bd0db
yoneda@73b1_desktop_22 <yoneda@jsk.imi.i.u-tokyo.ac.jp>
2025-07-07 14:24:57 +0900

    add speaker & update banner image

--- assets/js/index.js
+++ assets/js/index.js
@@ -103,8 +103,8 @@ $(document).ready(function () {
 
   // talk content
   talk_content = Object.values(talk_speaker_details)
-  populate_people_html('talk-content1', talk_content.slice(0, 4))
-  populate_people_html('talk-content2', talk_content.slice(4, ))
+  populate_people_html('talk-content1', talk_content.slice(0, 5))
+  populate_people_html('talk-content2', talk_content.slice(5, ))
 
   // organizers content
   populate_people_html('organizer-content-1', organizers_details.slice(0, 3))
